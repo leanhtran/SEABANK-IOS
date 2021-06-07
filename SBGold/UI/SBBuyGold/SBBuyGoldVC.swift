ed6a2f159b7f733f5a63d010553443d23a02d8cc
Anonymos <tuannm@mintpotvn.com>
2021-06-07 17:31:47 +0700

    update UI

--- SBGold/UI/SBBuyGold/SBBuyGoldVC.swift
+++ SBGold/UI/SBBuyGold/SBBuyGoldVC.swift
@@ -30,10 +30,12 @@ class SBBuyGoldVC: SBSGoldBaseVC {
   @IBOutlet weak private var transactionTypeView: InputTextView!
   @IBOutlet weak private var promotionView: InputTextView!
   
-  @IBOutlet weak var referUserRadioView: SBSRadioView!
+  @IBOutlet weak private var referUserRadioView: SBSRadioView!
   
-  @IBOutlet weak var transactionDateView: SBSInfoView!
-  @IBOutlet weak var policyView: SBSCheckBoxView!
+  
+  
+  @IBOutlet weak private var transactionDateView: SBSInfoView!
+  @IBOutlet weak private var policyView: SBSCheckBoxView!
   
   
   private var priceUnit: Int = 0 {
@@ -51,7 +53,8 @@ class SBBuyGoldVC: SBSGoldBaseVC {
     super.viewDidLoad()
     priceUnit = 5500000
     quantity = 1
-    appointmentDateView.setTitle(Date().toString(format: "yyyy/MM/dd"))
+    appointmentDateView.setContent(Date().toString(format: "yyyy/MM/dd"))
+    transactionDateView.setContent(Date().toString(format: "yyyy/MM/dd"))
   }
   
   override func setupView() {
@@ -59,6 +62,7 @@ class SBBuyGoldVC: SBSGoldBaseVC {
     setupMultiLanguage()
     setupNavigation()
     setupValueChange()
+    setupCommon()
   }
   
   private func setupNavigation() {
@@ -68,6 +72,15 @@ class SBBuyGoldVC: SBSGoldBaseVC {
   
   override func naviRightAction() {
     // todo right action
+  }
+  
+  private func setupCommon() {
+    Apply(referUserRadioView!) {
+      $0.layer.borderWidth = 1
+      $0.layer.borderColor = #colorLiteral(red: 0.8117647059, green: 0.8117647059, blue: 0.8117647059, alpha: 1)
+      $0.clipsToBounds = true
+      $0.layer.cornerRadius = 5
+    }
   }
   
   private func setupMultiLanguage() {
@@ -95,6 +108,19 @@ class SBBuyGoldVC: SBSGoldBaseVC {
     }
     transactionTypeView.title = Text.localizedString("BuyGod.TransactionClassify")
     promotionView.title = Text.localizedString("BuyGod.PromotionCode")
+    referUserRadioView.setRadioTitle([
+      Text.localizedString("BuyGod.Phone"),
+      Text.localizedString("BuyGod.CustomerCode")
+    ])
+    Apply(transactionDateView!) {
+      $0.setTitle(Text.localizedString("BuyGod.TransactionDate"))
+      $0.titleColor = #colorLiteral(red: 0.5725490196, green: 0.5803921569, blue: 0.5921568627, alpha: 1)
+      $0.contentFont = UIFont.boldSystemFont(ofSize: 14)
+    }
+    Apply(policyView!) {
+      $0.setTitle(Text.localizedString("BuyGod.BuyGod.Policy"))
+      $0.setMessage("")
+    }
   }
   
   private func setupValueChange() {
